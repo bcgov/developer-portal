3056e927c34d78d07d1cc6538b2279461136e8ec
Shea Phillips <shea.phillips@gmail.com>
2023-06-16 20:22:58 -0700

    add todo

--- packages/app/src/components/Root/LogoFull.tsx
+++ packages/app/src/components/Root/LogoFull.tsx
@@ -3,6 +3,7 @@ import BCLogo from './logo/BCID_H_rgb_rev.png';
 
 
 const LogoFull = () => {
+	// @todo fix this so we have a scalable format image
   return <img width="200px" alt="BC Mark" src={BCLogo}/>;
 };
 
