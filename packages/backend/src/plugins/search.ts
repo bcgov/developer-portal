da0cc214efcba323c2834d6f1dfead2599f0ef08
Shea Phillips <shea.phillips@gmail.com>
2023-07-04 21:38:24 -0700

    Switch to postgres search engine, if available.

--- packages/backend/src/plugins/search.ts
+++ packages/backend/src/plugins/search.ts
@@ -1,66 +1,68 @@
-import { useHotCleanup } from '@backstage/backend-common';
-import { createRouter } from '@backstage/plugin-search-backend';
+import {useHotCleanup} from '@backstage/backend-common';
+import {createRouter} from '@backstage/plugin-search-backend';
 import {
-  IndexBuilder,
-  LunrSearchEngine,
+	IndexBuilder,
+	LunrSearchEngine,
 } from '@backstage/plugin-search-backend-node';
-import { PluginEnvironment } from '../types';
-import { DefaultCatalogCollatorFactory } from '@backstage/plugin-catalog-backend';
-import { DefaultTechDocsCollatorFactory } from '@backstage/plugin-techdocs-backend';
-import { Router } from 'express';
+import {PluginEnvironment} from '../types';
+import {DefaultCatalogCollatorFactory} from '@backstage/plugin-catalog-backend';
+import {DefaultTechDocsCollatorFactory} from '@backstage/plugin-techdocs-backend';
+import {Router} from 'express';
+import {PgSearchEngine} from "@backstage/plugin-search-backend-module-pg";
 
 export default async function createPlugin(
-  env: PluginEnvironment,
+	env: PluginEnvironment,
 ): Promise<Router> {
-  // Initialize a connection to a search engine.
-  const searchEngine = new LunrSearchEngine({
-    logger: env.logger,
-  });
-  const indexBuilder = new IndexBuilder({
-    logger: env.logger,
-    searchEngine,
-  });
+	// Initialize a connection to a search engine.
+	const searchEngine = (await PgSearchEngine.supported(env.database))
+		? await PgSearchEngine.fromConfig(env.config, {database: env.database})
+		: new LunrSearchEngine({logger: env.logger});
 
-  const schedule = env.scheduler.createScheduledTaskRunner({
-    frequency: { minutes: 10 },
-    timeout: { minutes: 15 },
-    // A 3 second delay gives the backend server a chance to initialize before
-    // any collators are executed, which may attempt requests against the API.
-    initialDelay: { seconds: 3 },
-  });
+	const indexBuilder = new IndexBuilder({
+		logger: env.logger,
+		searchEngine,
+	});
 
-  // Collators are responsible for gathering documents known to plugins. This
-  // collator gathers entities from the software catalog.
-  indexBuilder.addCollator({
-    schedule,
-    factory: DefaultCatalogCollatorFactory.fromConfig(env.config, {
-      discovery: env.discovery,
-      tokenManager: env.tokenManager,
-    }),
-  });
+	const schedule = env.scheduler.createScheduledTaskRunner({
+		frequency: {minutes: 10},
+		timeout: {minutes: 15},
+		// A 3 second delay gives the backend server a chance to initialize before
+		// any collators are executed, which may attempt requests against the API.
+		initialDelay: {seconds: 3},
+	});
 
-  // collator gathers entities from techdocs.
-  indexBuilder.addCollator({
-    schedule,
-    factory: DefaultTechDocsCollatorFactory.fromConfig(env.config, {
-      discovery: env.discovery,
-      logger: env.logger,
-      tokenManager: env.tokenManager,
-    }),
-  });
+	// Collators are responsible for gathering documents known to plugins. This
+	// collator gathers entities from the software catalog.
+	indexBuilder.addCollator({
+		schedule,
+		factory: DefaultCatalogCollatorFactory.fromConfig(env.config, {
+			discovery: env.discovery,
+			tokenManager: env.tokenManager,
+		}),
+	});
 
-  // The scheduler controls when documents are gathered from collators and sent
-  // to the search engine for indexing.
-  const { scheduler } = await indexBuilder.build();
-  scheduler.start();
+	// collator gathers entities from techdocs.
+	indexBuilder.addCollator({
+		schedule,
+		factory: DefaultTechDocsCollatorFactory.fromConfig(env.config, {
+			discovery: env.discovery,
+			logger: env.logger,
+			tokenManager: env.tokenManager,
+		}),
+	});
 
-  useHotCleanup(module, () => scheduler.stop());
+	// The scheduler controls when documents are gathered from collators and sent
+	// to the search engine for indexing.
+	const {scheduler} = await indexBuilder.build();
+	scheduler.start();
 
-  return await createRouter({
-    engine: indexBuilder.getSearchEngine(),
-    types: indexBuilder.getDocumentTypes(),
-    permissions: env.permissions,
-    config: env.config,
-    logger: env.logger,
-  });
+	useHotCleanup(module, () => scheduler.stop());
+
+	return await createRouter({
+		engine: indexBuilder.getSearchEngine(),
+		types: indexBuilder.getDocumentTypes(),
+		permissions: env.permissions,
+		config: env.config,
+		logger: env.logger,
+	});
 }
