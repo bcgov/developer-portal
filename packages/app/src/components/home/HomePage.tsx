78f15c87a58dbecf3569e828f4529d50e8f272a3
Galen Gray <oomIRL@users.noreply.github.com>
2023-10-18 16:43:36 -0700

    DEVX-148: Improved homepage support links

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import styled, {createGlobalStyle} from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 import {useTheme} from '@material-ui/styles';
 import {ItemCardGrid, ItemCardHeader, LinkButton} from '@backstage/core-components';
 import {Theme} from '@material-ui/core/styles';
 import {HomePageSearchBar} from "@backstage/plugin-search";
 import {Card, CardActions, CardContent, CardMedia, makeStyles, Typography} from "@material-ui/core";
-import {HomePageToolkit} from "@backstage/plugin-home";
 import {GitHubSvgIcon, RocketChatIcon, StackOverFlowIcon} from "../utils/icons";
 import { Link } from 'react-router-dom';
 
@@ -29,15 +28,36 @@ const useStyles = makeStyles(theme => ({
 		gridTemplateColumns: 'repeat(auto-fill)',
 		gridGap: theme.spacing(3),
 	},
-	cardHeader: {
-		color: 'black',
+	cardEventHeader: {
+		color: 'white',
 		backgroundColor: '#0E3468',
 		backgroundImage: 'none',
 	},
 	card: {
+		color: theme.palette.primary.main,
 		display:'flex',
 		justiyContent:'space-between',
-		flexDirection:'column'	
+		flexDirection:'column',
+		borderRadius: '1rem',
+		boxShadow: 'rgba(0, 0, 0, .1) 0 20px 25px -5px, rgba(0, 0, 0, .04) 0 10px 10px -5px',
+	},
+	cardToolGrid: {
+		gridTemplateColumns: 'repeat(auto-fill)',
+		gridGap: theme.spacing(3),
+	},
+	cardToolHeader: {
+		color: 'black',
+		backgroundColor: theme.palette.background.paper,
+		backgroundImage: `linear-gradient(to bottom right, ${theme.palette.background.paper} 30%, rgba(0, 0, 0, .11))`
+	},
+	cardDocsHeader: {
+		color: theme.palette.primary.main,
+		backgroundColor: theme.palette.background.paper,
+		backgroundImage: 'none'
+	},
+	cardActions: {
+		justifyContent: 'flex-start',
+		padding: '1rem'
 	}
 }));
 makeStyles(theme => ({
@@ -79,46 +99,34 @@ const HomePage = () => {
 			margin-top: auto;
 			width: fit-content;
 			border-color: currentcolor;
-			border-bottom: 2px solid;
+			border-bottom: 2px solid rgba(0, 0, 0, .2);
 			border-radius: 0;
 			color: ${theme.palette.primary.main};
-			padding: calc(0.667em + 2px) 0 calc(0.33em + 2px);
+			padding: calc(0.667em + 4px) 4px calc(0.33em + 4px);
 			box-shadow: var(--box-shadow);
-		}
-	`;
-	const Container = styled.div`
-		box-shadow: var(--box-shadow);
-		border-radius: 1rem;
-		padding: 1rem;
-	`;
-
-	const ContainerH2 = styled.h2`
-		color: ${theme.palette.primary.main};
-	`;
-
-	const ContainerP = styled.p`
-		color: ${theme.palette.primary.main};
-	`;
-
-	const ContainerImg = styled.img`
-		width: 100%;
-	`;
+		}`;
 
 	const tools = [
 		{
 			url: 'https://stackoverflow.developer.gov.bc.ca',
 			label: 'Stack Overflow',
-			icon: <StackOverFlowIcon/>
+			icon: <StackOverFlowIcon/>,
+			buttonText: 'Ask a question',
+			desc: 'Ask, answer, and discuss technical questions on our own BC government instance of the popular Q & A platform.'
 		},
 		{
 			url: 'https://chat.developer.gov.bc.ca',
 			label: 'RocketChat',
-			icon: <RocketChatIcon/>
+			icon: <RocketChatIcon/>,
+			buttonText: 'Message teams',
+			desc: 'Connect on an open-source team communication app that offers real-time chat, file sharing, and collaboration features.'
 		},
 		{
 			url: 'https://github.com/bcgov',
 			label: 'GitHub',
-			icon: <GitHubSvgIcon/>
+			icon: <GitHubSvgIcon/>,
+			buttonText: 'Find code',
+			desc: 'Work together on a web-based version control platform that enables developers to host, review, and manage code repositories.'
 		}
 	]
 
@@ -156,52 +164,64 @@ const HomePage = () => {
 
 			<Grid container spacing={3}>
 				<Grid item sm={12} md={4} style={{display: 'flex'}}>
-					<Container style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start'}}>
-						<ContainerImg src="" />
-						<ContainerH2>
-							<Link to="docs/default/component/bcdg">
-								Application development guide
-							</Link>
-						</ContainerH2>
-						<ContainerP>Everything you need to know to build a quality, consistent and compliant application.</ContainerP>
-						<LinkButton
-							className="default-button"
-							title="bcdg"
-							to="docs/default/component/bcdg"
-						>Build a quality application</LinkButton>
-					</Container>
+					<Card key='bcdg' classes={{ root: classes.card }} >
+						<CardMedia>
+							<ItemCardHeader classes={{ root: classes.cardDocsHeader }} 
+								children={<Link to="docs/default/component/bcdg"><Typography variant='h5'>Application development guide</Typography></Link>} 
+							/>
+						</CardMedia>
+						<CardContent>
+							<Typography paragraph>
+							Everything you need to know to build a quality, consistent and compliant application.
+							</Typography>
+						</CardContent>
+						<CardActions classes={{ root: classes.cardActions }}>
+							<LinkButton to="docs/default/component/bcdg"
+								className="default-button"
+								title="bcdg"
+							>Build a quality application</LinkButton>
+						</CardActions>
+					</Card>
 				</Grid>
 				<Grid item sm={12} md={4} style={{display: 'flex'}}>
-					<Container style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start'}}>
-					<ContainerImg src="" />
-						<ContainerH2>
-							<Link to="docs/default/component/mobile-developer-guide">
-								Mobile development guide
-							</Link>
-						</ContainerH2>
-						<ContainerP>Detailed guidance on the steps and practices you must follow when developing a mobile application.</ContainerP>
-						<LinkButton
-							className="default-button"
-							title="mobile"
-							to="docs/default/component/mobile-developer-guide"
-						>Review the mobile development guide</LinkButton>
-					</Container>
+					<Card key='mobile-developer-guide' classes={{ root: classes.card }} >
+						<CardMedia>
+							<ItemCardHeader classes={{ root: classes.cardDocsHeader }} 
+								children={<Link to="docs/default/component/mobile-developer-guide"><Typography variant='h5'>Mobile development guide</Typography></Link>} 
+							/>
+						</CardMedia>
+						<CardContent>
+							<Typography paragraph>
+							Detailed guidance on the steps and practices you must follow when developing a mobile application.
+							</Typography>
+						</CardContent>
+						<CardActions classes={{ root: classes.cardActions }}>
+							<LinkButton to="docs/default/component/mobile-developer-guide"
+								className="default-button"
+								title="mobile-developer-guide"
+							>Review the mobile development guide</LinkButton>
+						</CardActions>
+					</Card>
 				</Grid>
 				<Grid item sm={12} md={4} style={{display: 'flex'}}>
-					<Container style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start'}}>
-						<ContainerImg src="" />
-						<ContainerH2>
-							<Link to="docs/default/component/platform-developer-docs">
-								Private cloud application deployment
-							</Link>
-						</ContainerH2>
-						<ContainerP>Learn how to deploy applications on the private cloud OpenShift environment.</ContainerP>
-						<LinkButton
-							className="default-button"
-							title="platform-developer-docs"
-							to="docs/default/component/platform-developer-docs"
-						>Get ready to deploy</LinkButton>
-					</Container>
+					<Card key='platform-developer-docs' classes={{ root: classes.card }} >
+						<CardMedia>
+							<ItemCardHeader classes={{ root: classes.cardDocsHeader }} 
+								children={<Link to="docs/default/component/platform-developer-docs"><Typography variant='h5'>Private cloud application deployment</Typography></Link>} 
+							/>
+						</CardMedia>
+						<CardContent>
+							<Typography paragraph>
+							Learn how to deploy applications on the private cloud OpenShift environment.
+							</Typography>
+						</CardContent>
+						<CardActions classes={{ root: classes.cardActions }}>
+							<LinkButton to="docs/default/component/platform-developer-docs"
+								className="default-button"
+								title="platform-developer-docs"
+							>Get ready to deploy</LinkButton>
+						</CardActions>
+					</Card>
 				</Grid>
 			</Grid>
 
@@ -216,7 +236,7 @@ const HomePage = () => {
 						<ItemCardHeader
 							title={`Application Modernization`}
 							subtitle="November 14th"
-							classes={{ root: classes.cardHeader }}
+							classes={{ root: classes.cardEventHeader }}
 						/>
 					</CardMedia>
 					<CardContent>
@@ -229,8 +249,8 @@ const HomePage = () => {
 						<br /><b>When: </b> Tuesday, November 14 · 8am - 3pm PT
 						</Typography>
 					</CardContent>
-					<CardActions>
-						<LinkButton color="primary" to="https://info.dynatrace.com/noram-great-lakes-dh-canada-west-government-day-23061-registration.html">
+					<CardActions classes={{ root: classes.cardActions }}>
+						<LinkButton to="https://info.dynatrace.com/noram-great-lakes-dh-canada-west-government-day-23061-registration.html" className="default-button">
 						Join the event
 						</LinkButton>
 					</CardActions>
@@ -240,7 +260,7 @@ const HomePage = () => {
 						<ItemCardHeader
 							title={`OpenShift 201 Workshop & Lab`}
 							subtitle="November 14th"
-							classes={{ root: classes.cardHeader }}
+							classes={{ root: classes.cardEventHeader }}
 						/>
 					</CardMedia>
 					<CardContent>
@@ -253,8 +273,8 @@ const HomePage = () => {
 						<br /><b>When: </b> Tuesday, November 14 · 9am - 4pm PT
 						</Typography>
 					</CardContent>
-					<CardActions>
-						<LinkButton color="primary" to="https://openshift201.eventbrite.com/">
+					<CardActions classes={{ root: classes.cardActions }}>
+						<LinkButton color="primary" to="https://openshift201.eventbrite.com/" className="default-button">
 						Register for OpenShift 201
 						</LinkButton>
 					</CardActions>
@@ -264,7 +284,7 @@ const HomePage = () => {
 						<ItemCardHeader
 							title={`OpenShift 101 Workshop & Lab`}
 							subtitle="November 21st"
-							classes={{ root: classes.cardHeader }}
+							classes={{ root: classes.cardEventHeader }}
 						/>
 					</CardMedia>
 					<CardContent>
@@ -277,22 +297,39 @@ const HomePage = () => {
 						<br /><b>When: </b>Tuesday, November 21 · 9am - 4pm PT
 						</Typography>
 					</CardContent>
-					<CardActions>
-						<LinkButton color="primary" to="https://openshift101.eventbrite.com/">
+					<CardActions classes={{ root: classes.cardActions }}>
+						<LinkButton color="primary" to="https://openshift101.eventbrite.com/" className="default-button">
 						Register for OpenShift 101
 						</LinkButton>
 					</CardActions>
 				</Card>
 			</ItemCardGrid>
 
-			<Box sx={{ pt: 7 }}>
-				<Grid item xs={12}>
-					<HomePageToolkit
-						title="Get support from the developer community"
-						tools={tools}
-					/>
-				</Grid>
+			<Box sx={{ pt: 6, pb: 1 }}>
+				<Typography variant="h3" gutterBottom>
+				Get support from the developer community
+				</Typography>
+				<Typography paragraph>
+				We're here to help! Ask questions on Stack Overflow, message product teams and developers on RocketChat and find code on GitHub.  
+				</Typography>
 			</Box>
+			<ItemCardGrid classes={{ root: classes.cardToolGrid }}>
+				{tools.map(t => (
+					<Card key={t.label} classes={{ root: classes.card }}>
+						<CardMedia>
+							<ItemCardHeader title={t.icon} children={t.label} classes={{ root: classes.cardToolHeader }} />
+						</CardMedia>
+						<CardContent>
+							<Typography paragraph>
+								{t.desc}
+							</Typography>
+						</CardContent>
+						<CardActions classes={{ root: classes.cardActions }}>
+							<LinkButton color='primary' to={t.url} className="default-button">{t.buttonText}</LinkButton>
+						</CardActions>
+					</Card>
+				))}
+			</ItemCardGrid>
 
 			<Box sx={{ pt: 5, pb: 4 }}>
 				<Box sx={{ pb: 1 }}>
