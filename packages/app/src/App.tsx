75aa5b4975976d813c73352cf384b118f7eff439
Monica Granbois <MonicaG@users.noreply.github.com>
2023-11-23 10:06:00 -0800

    Upgrade 16 (#58)
    Upgraded to version 1.16.0 of backstage.

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -48,7 +48,13 @@ const app = createApp({
 		bind(catalogPlugin.externalRoutes, {
 			createComponent: scaffolderPlugin.routes.root,
 			viewTechDoc: techdocsPlugin.routes.docRoot,
+			createFromTemplate: scaffolderPlugin.routes.selectedTemplate,
 		});
+
+		bind(scaffolderPlugin.externalRoutes, {
+			viewTechDoc: techdocsPlugin.routes.docRoot,
+		});
+
 		bind(orgPlugin.externalRoutes, {
 			catalogIndex: catalogPlugin.routes.catalogIndex,
 		});
