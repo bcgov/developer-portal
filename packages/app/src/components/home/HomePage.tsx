dfa85c01f70c317cdc3878ddf68dd5a49b14bb97
Monica Granbois <MonicaG@users.noreply.github.com>
2023-12-11 09:18:42 -0800

    test changes

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -189,7 +189,7 @@ const HomePage = () => {
 					<Grid container spacing={0} justifyContent='flex-start'>
 						<Grid item xs={12}>
 							<Typography paragraph>
-								The B.C. government DevHub is a place for developers and product teams to access common resources, tools, technical documentation and APIs.
+								Monica test changes - The B.C. government DevHub is a place for developers and product teams to access common resources, tools, technical documentation and APIs.
 							</Typography>
 						</Grid>
 					</Grid>
