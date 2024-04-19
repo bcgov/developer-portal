888811f10ab3aad7ccefd9e10ad10cb01b9f8ea6
Monica Granbois <MonicaG@users.noreply.github.com>
2024-04-19 11:30:51 -0700

    Initial commit. Has transfer logic

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,9 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { createHttpBackstageAction } from '@roadiehq/scaffolder-backend-module-http-request'
+import { ScmIntegrations } from '@backstage/integration';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -9,13 +11,28 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+
+ const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+
+  const actions = [...builtInActions, createHttpBackstageAction( { discovery: env.discovery })];
 
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
     reader: env.reader,
-    catalogClient,
+    catalogClient: catalogClient,
     identity: env.identity,
   });
+
+
+  
 }
