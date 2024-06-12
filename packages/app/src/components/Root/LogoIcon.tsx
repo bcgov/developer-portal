e24659c01cb3c1ba5268c747c9994d1a8ee83ebf
Galen Gray <oomIRL@users.noreply.github.com>
2024-06-11 17:23:05 -0700

    fixing case error with new icon

--- packages/app/src/components/Root/LogoIcon.tsx
+++ packages/app/src/components/Root/LogoIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DevHubIcon from './logo/DevhubIcon.png';
+import DevHubIcon from './logo/DevHubIcon.png';
 
 export const LogoIcon = () => {
   return <img width="64px" alt="DevHub Icon" src={DevHubIcon}/>;
