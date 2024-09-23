697f7af6f284d604d86d14022461e806706fce4f
Galen Gray <oomIRL@users.noreply.github.com>
2024-09-23 10:52:35 -0700

    Clarifying BC logo alt text

--- packages/app/src/components/Root/LogoIcon.tsx
+++ packages/app/src/components/Root/LogoIcon.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import DevHubIcon from './logo/BCID_V_RGB_rev.svg';
 
 export const LogoIcon = () => {
-  return <img width="64px" height="auto" alt="DevHub Icon" src={DevHubIcon}/>;
+  return <img width="64px" height="auto" alt="Government of B.C. logo" src={DevHubIcon}/>;
 };
