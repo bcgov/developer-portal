dad9b959590d109a4f28a47cbe1c330766dcfc4d
Min Kim <minkimcello@gmail.com>
2025-02-24 22:04:06 -0500

    Add policy rows in card for components

--- packages/app/src/components/catalog/EntityPoliciesCard/EntityPoliciesCard.tsx
+++ packages/app/src/components/catalog/EntityPoliciesCard/EntityPoliciesCard.tsx
@@ -6,9 +6,11 @@ import Divider from '@material-ui/core/Divider';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import { InfoCardVariants } from '@backstage/core-components';
 import { makeStyles } from '@material-ui/core/styles';
 import { useEntity } from '@backstage/plugin-catalog-react';
+import { Link } from '@material-ui/core';
 
 interface PoliciesCardProps {
   variant?: InfoCardVariants;
@@ -46,7 +48,50 @@ const useStyles = makeStyles({
   cardContent: {
     flex: 1,
   },
+  policyRowContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: '10px',
+    marginBottom: '12px',
+  },
+  policyRowContent: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  policyRowDescription: {
+    marginLeft: '15px',
+  },
+  policyRowCheckbox: {
+    fill: '#00be00',
+    fontSize: 40,
+  },
 });
+
+const exampleComponentPolicies = [
+  'Component performs dependency chain analysis',
+  'Container vulnerability scanning report is 34 days old',
+  'Repository uses an automated dependency update tool',
+  'No crticial alerts older than 30 days',
+  'Repository does not contain unencrypted secrets',
+];
+
+const PolicyRow = ({ description }: { description: string }) => {
+  const classes = useStyles();
+  return (
+    <Grid className={classes.policyRowContainer}>
+      <Grid className={classes.policyRowContent}>
+        <CheckBoxIcon className={classes.policyRowCheckbox} />
+        <Typography className={classes.policyRowDescription}>
+          {description}
+        </Typography>
+      </Grid>
+      <Link style={{ cursor: 'pointer' }} variant="body2">
+        Learn more
+      </Link>
+    </Grid>
+  );
+};
 
 export function EntityPoliciesCard(props: PoliciesCardProps) {
   const { variant } = props;
@@ -74,7 +119,14 @@ export function EntityPoliciesCard(props: PoliciesCardProps) {
         }
       />
       <Divider />
-      <CardContent className={classes.cardContent} />
+      <CardContent className={classes.cardContent}>
+        {exampleComponentPolicies.map(policy => (
+          <>
+            <PolicyRow description={policy} />
+            <Divider variant="middle" />
+          </>
+        ))}
+      </CardContent>
     </Card>
   );
 }
