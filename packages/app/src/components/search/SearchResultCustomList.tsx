ec459aeffcaccc6d93aff34760975e4f124803a0
Galen Gray <oomIRL@users.noreply.github.com>
2025-06-16 16:46:19 -0700

    switch search results from inline definition to interface

--- packages/app/src/components/search/SearchResultCustomList.tsx
+++ packages/app/src/components/search/SearchResultCustomList.tsx
@@ -15,73 +15,73 @@ import { GitHubSvgIcon } from '../utils/icons';
 import { TechDocsSearchResultCustomListItem } from './TechDocsSearchResultCustomListItem';
 import { ResultHighlight } from '@backstage/plugin-search-common/index';
 
+interface SearchResultItem {
+  type: string;
+  document: any;
+  highlight?: ResultHighlight;
+  rank?: number;
+}
+
 const SearchResultCustomList = () => {
   return (
     <SearchResult>
       {({ results }) => (
         <List>
-          {results.map(
-            (result: {
-              type: string;
-              document: any;
-              highlight?: ResultHighlight;
-              rank?: number;
-            }) => {
-              const { type, document, highlight, rank } = result;
-              switch (type) {
-                case 'software-catalog':
-                  return (
-                    <CatalogSearchResultListItem
-                      key={document.location}
-                      result={document}
-                      highlight={highlight}
-                      rank={rank}
-                      icon={<CatalogIcon />}
-                    />
-                  );
-                case 'techdocs':
-                  return (
-                    <TechDocsSearchResultCustomListItem
-                      key={document.location}
-                      result={document}
-                      highlight={highlight}
-                      rank={rank}
-                      asListItem
-                      icon={<DocsIcon />}
-                    />
-                  );
-                case 'stack-overflow':
-                  return (
-                    <StackOverflowSearchResultListItem
-                      key={document.location}
-                      result={document}
-                      highlight={highlight}
-                      rank={rank}
-                      icon={<StackOverflowIcon />}
-                    />
-                  );
-                case 'github-discussions':
-                  return (
-                    <GithubDiscussionsSearchResultListItem
-                      key={document.location}
-                      result={document}
-                      highlight={highlight}
-                      rank={rank}
-                      icon={<GitHubSvgIcon />}
-                    />
-                  );
-                default:
-                  return (
-                    <DefaultResultListItem
-                      key={document.location}
-                      result={document}
-                      highlight={highlight}
-                      rank={rank}
-                    />
-                  );
-              }
-            },
-          )}
+          {results.map((result: SearchResultItem) => {
+            const { type, document, highlight, rank } = result;
+            switch (type) {
+              case 'software-catalog':
+                return (
+                  <CatalogSearchResultListItem
+                    key={document.location}
+                    result={document}
+                    highlight={highlight}
+                    rank={rank}
+                    icon={<CatalogIcon />}
+                  />
+                );
+              case 'techdocs':
+                return (
+                  <TechDocsSearchResultCustomListItem
+                    key={document.location}
+                    result={document}
+                    highlight={highlight}
+                    rank={rank}
+                    asListItem
+                    icon={<DocsIcon />}
+                  />
+                );
+              case 'stack-overflow':
+                return (
+                  <StackOverflowSearchResultListItem
+                    key={document.location}
+                    result={document}
+                    highlight={highlight}
+                    rank={rank}
+                    icon={<StackOverflowIcon />}
+                  />
+                );
+              case 'github-discussions':
+                return (
+                  <GithubDiscussionsSearchResultListItem
+                    key={document.location}
+                    result={document}
+                    highlight={highlight}
+                    rank={rank}
+                    icon={<GitHubSvgIcon />}
+                  />
+                );
+              default:
+                return (
+                  <DefaultResultListItem
+                    key={document.location}
+                    result={document}
+                    highlight={highlight}
+                    rank={rank}
+                  />
+                );
+            }
+          })}
         </List>
       )}
     </SearchResult>
